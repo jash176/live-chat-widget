ae9d3352fcb581ee2779cae5560e6c662ac18d03
jash <jashpalavwala39@gmail.com>
2025-04-17 21:04:30 +0530

    urls changed

--- src/utils/generalUrls.js
+++ src/utils/generalUrls.js
@@ -1,5 +1,11 @@
 export const URLS = {
-  baseUrl: "http://localhost:5000/api",
-  assetsUrl: "http://localhost:5000",
-  socket: "http://127.0.0.1:5000/",
+  baseUrl: "https://supportly.site/api",
+  assetsUrl: "https://supportly.site",
+  socket: "https://supportly.site/",
 };
+
+// export const URLS = {
+//   baseUrl: "http://localhost:5000/api",
+//   assetsUrl: "http://localhost:5000",
+//   socket: "http://127.0.0.1:5000/",
+// }
